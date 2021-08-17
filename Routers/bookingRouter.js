91ea063fbb0381a012756fefa5864f46880407e4
Kushwaha-abhay <72202582+Kushwaha-abhay@users.noreply.github.com>
2021-08-18 01:11:42 +0530

    final

--- Routers/bookingRouter.js
+++ Routers/bookingRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { protectRoute } = require("../Controller/authController");
 const { createPaymentSesion, checkoutComplete } = require("../Controller/bookingController");
-
+const bodyParser = require("body-parser");
 const bookingRouter = express.Router();
 // customer : user.name,
 //     customer_email : user.email,
